c3c754f2a767fcd8cb6d44746c72edb8e6b67160
Gabriel F Albuquerque <gfonteles.a@gmail.com>
2023-04-12 17:16:18 -0300

    add classes curso and mentoria

--- desafio-poo-dio/src/App.java
+++ desafio-poo-dio/src/App.java
@@ -1,5 +1,16 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Curso curso1 = new Curso("Java", "Descrição curso Java", 8);
+
+        Curso curso2 = new Curso("Javascript", "Descrição curso Javascript", 20);
+
+        Mentoria mentoria = new Mentoria("Masterclass", "Descrição Masterclass", LocalDate.now().plusDays(10));
+
+        System.out.println(mentoria.getData());
     }
 }
